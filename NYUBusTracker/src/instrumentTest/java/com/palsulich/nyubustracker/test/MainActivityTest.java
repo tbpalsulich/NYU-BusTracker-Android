f4e13c32ad034df1efc08e06f551dd3267ae26df
Tyler Palsulich <tpalsulich@gmail.com>
2014-02-05 00:11:20 -0500

    First test file.

---
+++ NYUBusTracker/src/instrumentTest/java/com/palsulich/nyubustracker/test/MainActivityTest.java
@@ -0,0 +1,11 @@
+package com.palsulich.nyubustracker.test;
+
+import android.test.ActivityTestCase;
+
+import junit.framework.Assert;
+
+public class MainActivityTest extends ActivityTestCase {
+    public void testHappy(){
+        Assert.assertTrue(true);
+    }
+}
