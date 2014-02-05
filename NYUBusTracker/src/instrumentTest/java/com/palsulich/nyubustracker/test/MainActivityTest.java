551b7c68dde3acaa4301a4ef6b0ca2e1c7b97380
Tyler Palsulich <tpalsulich@gmail.com>
2014-02-05 00:48:54 -0500

    Slightly better testing.

--- NYUBusTracker/src/instrumentTest/java/com/palsulich/nyubustracker/test/MainActivityTest.java
+++ NYUBusTracker/src/instrumentTest/java/com/palsulich/nyubustracker/test/MainActivityTest.java
@@ -1,11 +1,43 @@
 package com.palsulich.nyubustracker.test;
 
-import android.test.ActivityTestCase;
+import android.app.Activity;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
 
-import junit.framework.Assert;
+import com.palsulich.nyubustracker.R;
+import com.palsulich.nyubustracker.activities.MainActivity;
 
-public class MainActivityTest extends ActivityTestCase {
-    public void testHappy(){
-        Assert.assertTrue(true);
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+    Activity mActivity;
+    TextView startText;
+    String startTextCorrect;
+    TextView endText;
+    String endTextCorrect;
+
+    public MainActivityTest(){
+        super(MainActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mActivity = this.getActivity();
+        startText = (TextView) mActivity.findViewById(R.id.start);
+        startTextCorrect = mActivity.getString(R.string.start);
+        endText = (TextView) mActivity.findViewById(R.id.end);
+        endTextCorrect = mActivity.getString(R.string.end);
+    }
+
+    public void testPreconditions() {
+        assertNotNull(startText);
+        assertNotNull(endText);
+    }
+
+    public void testStartText() {
+        assertEquals(startTextCorrect, (String)startText.getText());
+    }
+
+    public void testEndText() {
+        assertEquals(endTextCorrect, (String) endText.getText());
     }
 }
