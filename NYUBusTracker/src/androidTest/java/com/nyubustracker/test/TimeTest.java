d5a1201129c3da317c6bbc2c75729fbc365c6eeb
Tyler Palsulich <tpalsulich@gmail.com>
2014-09-01 22:52:55 -0400

    Test the Time comparator.

--- NYUBusTracker/src/androidTest/java/com/nyubustracker/test/TimeTest.java
+++ NYUBusTracker/src/androidTest/java/com/nyubustracker/test/TimeTest.java
@@ -4,6 +4,10 @@ import com.nyubustracker.models.Time;
 
 import junit.framework.TestCase;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class TimeTest extends TestCase{
     public void testAMConstructor() {
         Time t = new Time("12:10 AM", Time.TimeOfWeek.Weekday, "Route T");
@@ -51,5 +55,41 @@ public class TimeTest extends TestCase{
         assertEquals("Bad format: Hour should be set", 18, t.getHour());
         assertEquals("Bad format: Minute should be set", 21, t.getMinute());
         assertFalse("Bad format: AM/PM should be set", t.isAM());
+
+        t = new Time(" 6 : 21       Pm   ", Time.TimeOfWeek.Weekday, "Route T");
+        assertEquals("Bad format: Hour should be set", 18, t.getHour());
+        assertEquals("Bad format: Minute should be set", 21, t.getMinute());
+        assertFalse("Bad format: AM/PM should be set", t.isAM());
+    }
+
+    public void testComparatorSameDay() {
+        Time t0 = new Time("12:00 AM", Time.TimeOfWeek.Weekday, "Route T");
+        Time t1 = new Time("12:01 AM", Time.TimeOfWeek.Weekday, "Route T");
+        Time t2 = new Time( "1:00 AM", Time.TimeOfWeek.Weekday, "Route T");
+        Time t3 = new Time( "1:00 PM", Time.TimeOfWeek.Weekday, "Route T");
+        assertTrue(Time.compare.compare(t0, t1) < 0);
+        assertTrue(Time.compare.compare(t0, t2) < 0);
+        assertTrue(Time.compare.compare(t2, t1) > 0);
+        assertTrue(Time.compare.compare(t3, t1) > 0);
+    }
+
+    public void testComparatorDifferentDay() {
+        Time t0 = new Time("12:00 AM", Time.TimeOfWeek.Weekday, "Route T");
+        Time t1 = new Time("12:01 AM", Time.TimeOfWeek.Weekday, "Route T");
+        Time t2 = new Time( "1:00 AM", Time.TimeOfWeek.Friday, "Route T");
+        Time t3 = new Time( "1:00 PM", Time.TimeOfWeek.Weekend, "Route T");
+        assertTrue(Time.compare.compare(t0, t1) < 0);
+        assertTrue(Time.compare.compare(t0, t2) < 0);
+        assertTrue(Time.compare.compare(t2, t1) > 0);
+        assertTrue(Time.compare.compare(t3, t1) > 0);
+        assertTrue(Time.compare.compare(t3, t2) > 0);
+        assertTrue(Time.compare.compare(t2, t3) < 0);
+    }
+
+    public void testComparatorEquals() {
+        Time t0 = new Time( "1:00 PM", Time.TimeOfWeek.Weekend, "Route T");
+        Time t1 = new Time( "1:00 PM", Time.TimeOfWeek.Weekend, "Route T");
+        assertTrue(Time.compare.compare(t0, t1) == 0);
+        assertEquals(t0, t1);
     }
 }
